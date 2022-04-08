cf9519a44bd27f4c61b89b8672abb3d2d7622571
unknown <ganitniduniyanikunj10@gmail.com>
2022-04-08 11:45:30 +0530

    mphasis 08-04-2022 session notes

--- mphasis 08-04-2022/parent/src/app/child/child.component.ts
+++ mphasis 08-04-2022/parent/src/app/child/child.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.css'],
   inputs:['pdata'],
+  outputs:['cevent'],
 })
 export class ChildComponent implements OnInit {
 
@@ -14,5 +15,12 @@ export class ChildComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  cevent=new EventEmitter<string>();
+
+  onchange(value:string){
+    this.cevent.emit(value);
+  }
+
 
 }
