d3938b47828adee0ed923fd3e2ed783aee81d8bc
unknown <ganitniduniyanikunj10@gmail.com>
2022-07-29 16:43:38 +0530

    Backend+frontend integration

---
+++ Capstone Project 17-07-2022/SpringBootCRUD/src/main/java/com/simplilearn/demo/service/UserService.java
@@ -0,0 +1,60 @@
+package com.simplilearn.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.simplilearn.demo.entity.User;
+import com.simplilearn.demo.repo.UserRepo;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepo repo;
+	
+	public User addUser(User u) {
+		return repo.save(u);
+	}
+	public List<User> getAllUser(){
+		return repo.findAll();
+	}
+	public User getUserById(int id) {
+		if(repo.findById(id).isPresent()) {
+			return repo.findById(id).get();
+		}
+		else {
+			return null;
+		}
+		
+	}
+	public User updateUser(int id, User newUser) {
+		if(repo.findById(id).isPresent()) {
+			User olduser= repo.findById(id).get();
+			olduser.setName(newUser.getName());
+			olduser.setEmail(newUser.getEmail());
+			olduser.setCountry(newUser.getCountry());
+			
+			return repo.save(olduser);
+		}
+		else {
+			return null;
+		}
+	}
+	
+	
+	public boolean deleteUser(int id) {
+		if(repo.findById(id).isPresent()) {
+			repo.deleteById(id);
+			return true;
+		}
+		
+		else {
+			return false;
+		}
+	}
+	
+	
+
+}
